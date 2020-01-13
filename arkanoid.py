d6c24c37c6e51ce7ddee671801d19584b2cff63f
fanu1k <golenev.nickita@gmail.com>
2020-01-13 20:13:39 +0300

    new game
    arkanoid v1.1

---
+++ arkanoid.py
@@ -0,0 +1,277 @@
+import math
+import pygame
+from random import randint as rnd
+from time import sleep
+block_width = 25
+block_height = 15
+
+
+
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, color, x, y, lifes):
+    
+        super().__init__()
+    
+        self.image = pygame.Surface([block_width, block_height])
+
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+
+    
+        self.rect.x = x
+        self.rect.y = y
+        self.lifes = lifes
+
+    def kill_life(self):
+        self.lifes -= 1
+        if self.lifes == 1:
+            self.image.fill(pygame.Color('red'))
+        elif self.lifes == 2:
+            self.image.fill(pygame.Color('green'))
+        elif self.lifes == 3:
+            self.image.fill(pygame.Color('orange'))
+        return self.lifes
+
+
+class Ball(pygame.sprite.Sprite):
+
+    x = 0.0
+    y = 170.0
+
+    direction = 220
+
+    width = 10
+    height = 10
+
+
+    def __init__(self):
+        super().__init__()
+
+    
+        self.image = pygame.Surface([self.width, self.height])
+
+    
+        self.image.fill(pygame.Color('white'))
+
+    
+        self.rect = self.image.get_rect()
+
+    
+        self.screenheight = pygame.display.get_surface().get_height()
+        self.screenwidth = pygame.display.get_surface().get_width()
+
+        self.speed = 7
+
+    def nxt_lvl(self, lvl):
+        self.speed = 7 + 1 * lvl
+
+    def bounce(self, diff):
+    
+
+        self.direction = (180 - self.direction) % 360
+        self.direction -= diff
+
+    def update(self):
+    
+    
+        direction_radians = math.radians(self.direction)
+
+    
+        self.x += self.speed * math.sin(direction_radians)
+        self.y -= self.speed * math.cos(direction_radians)
+
+    
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    
+        if self.y <= 0:
+            self.bounce(0)
+            self.y = 1
+
+    
+        if self.x <= 0:
+            self.direction = (360 - self.direction) % 360
+            self.x = 1
+
+    
+        if self.x > self.screenwidth - self.width:
+            self.direction = (360 - self.direction) % 360
+            self.x = self.screenwidth - self.width - 1
+
+    
+        if self.y > 600:
+            return True
+        else:
+            return False
+
+
+class Player(pygame.sprite.Sprite):
+
+
+    def __init__(self):
+    
+        super().__init__()
+
+        self.width = 75
+        self.height = 15
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill((pygame.Color('white')))
+
+        self.rect = self.image.get_rect()
+        self.screenheight = pygame.display.get_surface().get_height()
+        self.screenwidth = pygame.display.get_surface().get_width()
+
+        self.rect.x = 0
+        self.rect.y = self.screenheight-self.height
+
+    def update(self):
+    
+    
+        pos = pygame.mouse.get_pos()
+        self.rect.x = pos[0]
+        if self.rect.x > self.screenwidth - self.width:
+            self.rect.x = self.screenwidth - self.width
+
+
+def main(lvl):
+    pygame.init()
+
+    life = 3
+
+    screen = pygame.display.set_mode([800, 600])
+
+    pygame.display.set_caption('Arkanoid')
+
+
+    pygame.mouse.set_visible(0)
+
+
+    font = pygame.font.Font(None, 36)
+
+
+    background = pygame.Surface(screen.get_size())
+
+
+    blocks = pygame.sprite.Group()
+    balls = pygame.sprite.Group()
+    allsprites = pygame.sprite.Group()
+    two_lvl_blocks = pygame.sprite.Group()
+    three_lvl_blocks = pygame.sprite.Group()
+
+
+    player = Player()
+    allsprites.add(player)
+
+
+    ball = Ball()
+    allsprites.add(ball)
+    balls.add(ball)
+    ball.nxt_lvl(lvl)
+
+
+    top = 80
+
+
+    blockcount = 32
+
+    for row in range(5):
+        for column in range(0, blockcount):
+        
+            if rnd(0, 100) < (7 * lvl):
+                block = Block(pygame.Color('orange'), column *
+                              (block_width + 2) + 1, top, 3)
+                three_lvl_blocks.add(block)
+            elif rnd(0, 100) < (10 * lvl):
+                block = Block(pygame.Color('green'), column *
+                              (block_width + 2) + 1, top, 2)
+                two_lvl_blocks.add(block)
+            else:
+                block = Block(pygame.Color('red'), column *
+                              (block_width + 2) + 1, top, 1)
+                blocks.add(block)
+            allsprites.add(block)
+        top += block_height + 2
+
+    clock = pygame.time.Clock()
+
+    game_over = False
+
+    exit_program = False
+
+    while not exit_program:
+        screen.fill(pygame.Color('black'))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_program = True
+
+        if not game_over:
+            player.update()
+            game_over = ball.update()
+
+        if game_over:
+            if life:
+                life -= 1
+                game_over = False
+                ball.kill()
+                ball = Ball()
+                allsprites.add(ball)
+                balls.add(ball)
+                text = font.render(f"Lifes:{life}", True, pygame.Color('white'))
+                textpos = text.get_rect(centerx=background.get_width() / 2)
+                textpos.top = 300
+                screen.blit(text, textpos)
+                pygame.display.flip()
+                sleep(2)
+            else:
+                text = font.render("Game Over", True, pygame.Color('white'))
+                textpos = text.get_rect(centerx=background.get_width() / 2)
+                textpos.top = 300
+                screen.blit(text, textpos)
+                pygame.display.flip()
+                sleep(2)
+                return False
+
+        if pygame.sprite.spritecollide(player, balls, False):
+        
+            diff = (player.rect.x + player.width / 2) - \
+                (ball.rect.x + ball.width / 2)
+            ball.rect.y = screen.get_height() - player.rect.height - ball.rect.height - 1
+            ball.bounce(diff)
+
+    
+        deadblocks = pygame.sprite.spritecollide(ball, blocks, False)
+        two_lvl_deadblocks = pygame.sprite.spritecollide(ball, two_lvl_blocks, False)
+        three_lvl_deadblocks = pygame.sprite.spritecollide(ball, three_lvl_blocks, False)
+    
+        if len(deadblocks) > 0:
+            ball.bounce(0)
+            for i in range(len(deadblocks)):
+                if deadblocks[i].kill_life() == 0:
+                    deadblocks[i].kill()
+        if len(two_lvl_deadblocks) > 0:
+            ball.bounce(0)
+            for i in range(len(two_lvl_deadblocks)):
+                if two_lvl_deadblocks[i].kill_life() == 0:
+                    two_lvl_deadblocks[i].kill()
+        if len(three_lvl_deadblocks) > 0:
+            ball.bounce(0)
+            for i in range(len(three_lvl_deadblocks)):
+                if three_lvl_deadblocks[i].kill_life() == 0:
+                    three_lvl_deadblocks[i].kill()
+    
+        if len(blocks) + len(two_lvl_blocks) + len(three_lvl_blocks) == 0:
+            return True
+
+        allsprites.draw(screen)
+        clock.tick(30)
+        pygame.display.flip()
+
+    pygame.quit()
+
+if '__main__' == __name__:
+    for i in range(0, 10):
+        if not main(i):
+            break
